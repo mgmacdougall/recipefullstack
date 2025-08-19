77b7380e6a31a8d59a2766d714d535eb3f4c3c50
Mike MacDougall <mgmacdougall@outlook.com>
2025-08-19 10:21:34 -0400

    Add of authentication + example

--- api/src/index.js
+++ api/src/index.js
@@ -1,5 +1,10 @@
 import express from 'express';
 import cors from 'cors';
+import session from 'express-session';
+
+
+import bodyParser from 'body-parser';
+
 import logger from './utils/logger.js';
 import morgan from 'morgan';
 import router from './routes/recipes.js';
@@ -15,6 +20,12 @@ connectDB();
 app.use(express.json());
 app.use(cors())
 app.use(morgan('combined', { stream: { write: message => logger.info(message.trim()) } }));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(session({
+  secret: process.env.SECRET_KEY || 'defaultsecret',
+  resave: false,
+  saveUninitialized: false
+}));
 const PORT = 3000;
 
 
