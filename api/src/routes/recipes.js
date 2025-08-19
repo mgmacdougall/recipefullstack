fede081981ce999140099407f81b6b1533bd886d
Mike MacDougall <mgmacdougall@outlook.com>
2025-08-19 10:26:24 -0400

    Add recipe add, suggestion routes

--- api/src/routes/recipes.js
+++ api/src/routes/recipes.js
@@ -1,7 +1,10 @@
 import express from 'express';
 import logger from '../utils/logger.js';
+
 const router = express.Router();
 import recipeController from '../controllers/recipeController.js';
+
+
 
 router.get('/', (req, res) => {
     logger.info('Received a GET request on /recipes');
@@ -14,6 +17,14 @@ router.get('/:id', async (req, res) => {
 
 router.delete('/delete/:id', async (req, res) => {
     recipeController.getOneRecipeAndDelete(req, res);
+})
+
+router.post('/add', async (req, res) => {
+    recipeController.addNewRecipe(req, res);
+})
+
+router.post('/suggestion', async (req, res) => {
+    recipeController.getSuggestion(req, res);
 })
 
 export default router;
