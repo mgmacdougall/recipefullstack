9091c9f533b4683aa3599e7510750795b60ec136
Mike MacDougall <mgmacdougall@outlook.com>
2025-08-22 15:56:18 -0400

    Add of favourite to Recipe card.

--- frontend/src/components/RecipeCard.jsx
+++ frontend/src/components/RecipeCard.jsx
@@ -2,15 +2,18 @@ import React from 'react'
 import { v4 as uuidv4 } from 'uuid';
 
 import './RecipeCard.css'
-
-function RecipeCard({ data }) {
-
+import RecipeCardFavorite from './RecipeCardFavorite.jsx';
+function RecipeCard({ data,recipeFavouriteHandler }) {
+    console.log('RecipeCard data:', data);
     return (
         <div className="recipe-container">
             {
                 data.map((recipe) => (
-                    <div key={recipe._id} className="recipe-card">
+                    <div key={recipe._id} className="recipe-card" id={recipe._id}>
                         <h2 className='text-base text-5xl font-serif font-bold'>{recipe.title}</h2>
+                        <RecipeCardFavorite handler={recipeFavouriteHandler} favouriteId={recipe._id}/>
+                    
+                        <p>{recipe.favourite? "favourite":"not favorite"}</p>
                         <p className='text-base text-gray-700 leading-relaxed font-serif p-4'>Hard coded for now</p>
                         <h3 className='text-base text-3xl font-serif font-bold'>Ingredients:</h3>
                         <ul>
