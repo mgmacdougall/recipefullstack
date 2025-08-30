9e03b02e3ae459e24513626b9f65219abfb05964
Mike MacDougall <mgmacdougall@outlook.com>
2025-08-30 17:17:30 -0400

    Add of font awesome.

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import NavBar from '../components/NavBar'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faHouse } from '@fortawesome/free-solid-svg-icons'
 function Home() {
 
     return (
         <div>
             <NavBar/>
-            <h1 className='text-4xl font-extrabold tracking-tight text-gray-900 sm:text-5xl md:text-6xl'>Home</h1>
+            <h1 className='text-4xl font-extrabold tracking-tight text-gray-900 sm:text-5xl md:text-6xl'><FontAwesomeIcon icon={faHouse} />Home</h1>
         </div>
     )
 }
