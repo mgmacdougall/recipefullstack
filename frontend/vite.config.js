a6d770237b27eaa0eff76cdc1ee4170a26954935
Mike MacDougall <mgmacdougall@outlook.com>
2025-08-31 17:40:14 -0400

    Add of logger information to vite config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// vite.config.js
 export default defineConfig({
-  plugins: [react()],
-})
+   plugins: [react()],
+  define: {
+    'process.env': {
+      REACT_APP_API_URL: '"https://api.example.com"',
+    },
+  },
+});
