c47c08ab3785703da01aa218c87593b4416171b7
Mike MacDougall <mgmacdougall@outlook.com>
2025-08-31 17:41:48 -0400

    Add of id for labels and inputs

--- frontend/src/components/RecipeForm.jsx
+++ frontend/src/components/RecipeForm.jsx
@@ -1,24 +1,24 @@
+import React from 'react'
+import { logger } from '../utils/logger';
+
+
 function RecipeForm({recipeTitleHandler, recipeTitleData, recipeQuanityHandler, quantityData, ingredientChangeHandler, ingredientData,instructionsHandler, instructionsData,handleRecipeFormSubmit}) {
 
-    console.log(recipeTitleData);
-    console.log(quantityData);
-    console.log(ingredientData);
-    console.log(instructionsData);
     return (
         <div className="mb-6">
             <h2>Enter recipe</h2>
             <form className="max-w-sm mx-auto" onSubmit={handleRecipeFormSubmit}>
                 <div className="md:flex md:items-center mb-6">
                     <label htmlFor="title" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Title</label>
-                    <input type="text" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" name="title" placeholder="Recipe Title" onChange={recipeTitleHandler} value={recipeTitleData} />
+                    <input id="title" type="text" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" name="title" placeholder="Recipe Title" onChange={recipeTitleHandler} value={recipeTitleData} />
                 </div>
                 <div className="md:flex md:items-center mb-6">
                     <label htmlFor="quantity" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Quantity</label>
-                    <input type="text" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" name="quantity" placeholder="Quantity" onChange={recipeQuanityHandler} value={quantityData}/>
+                    <input id="quantity" type="text" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" name="quantity" placeholder="Quantity" onChange={recipeQuanityHandler} value={quantityData}/>
                 </div>
                 <div className="md:flex md:items-center mb-6">
                     <label htmlFor="ingredient" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Ingredient</label>
-                    <input type="text" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" name="ingredient" placeholder="Ingredient" onChange={ingredientChangeHandler} value={ingredientData}/>
+                    <input id="ingredient" type="text" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" name="ingredient" placeholder="Ingredient" onChange={ingredientChangeHandler} value={ingredientData}/>
                 </div>
                 <div className="md:flex md:items-center mb-6">
                     <label htmlFor="instructions" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">instructions</label>
