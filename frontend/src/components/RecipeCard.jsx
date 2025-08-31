493afcc1a5d28f58b7748a8b6b2bb26ec059e4cf
Mike MacDougall <mgmacdougall@outlook.com>
2025-08-31 17:41:01 -0400

    Update of css for component

--- frontend/src/components/RecipeCard.jsx
+++ frontend/src/components/RecipeCard.jsx
@@ -3,8 +3,8 @@ import { v4 as uuidv4 } from 'uuid';
 
 import './RecipeCard.css'
 import RecipeCardFavorite from './RecipeCardFavorite.jsx';
-function RecipeCard({ data,recipeFavouriteHandler }) {
-    console.log('RecipeCard data:', data);
+function RecipeCard({ data, recipeFavouriteHandler }) {
+    console.info('RecipeCard data:', data);
     return (
         <div className="recipe-container">
             {
@@ -13,11 +13,11 @@ function RecipeCard({ data,recipeFavouriteHandler }) {
                         <h2 className='text-base text-5xl font-serif font-bold'>{recipe.title}</h2>
                         <RecipeCardFavorite handler={recipeFavouriteHandler} favouriteId={recipe._id}/>
                     
-                        <p>{recipe.favourite? "favourite":"not favorite"}</p>
-                        <p className='text-base text-gray-700 leading-relaxed font-serif p-4'>Hard coded for now</p>
+                        <p className={`activity-toggle ${recipe.isFavorite?'active':''}`}>{recipe.favourite? "favourite":"not favorite"}</p>
+                        <p className='text-base text-gray-700 leading-relaxed font-serif p-4'>{recipe.quantity}</p>
                         <h3 className='text-base text-3xl font-serif font-bold'>Ingredients:</h3>
                         <ul>
-                            {recipe.ingredients.map(ingredient => <li key={ingredient._id} >{ingredient.name} {ingredient.quantity}</li>)}
+                            {recipe.ingredients.map(ingredient => <li key={ingredient._id} >{ingredient.name} {ingredient.quantity} </li>)}
                         </ul>         
                     </div>
                 ))
