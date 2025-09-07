3256e508fbd97650222cdd1ea7cac22e9180846f
Mike MacDougall <mgmacdougall@outlook.com>
2025-09-07 14:48:47 -0400

    Remove of substitutions from meal planner page

--- frontend/src/pages/MealPlanner.jsx
+++ frontend/src/pages/MealPlanner.jsx
@@ -1,18 +1,19 @@
-import {React, useState} from 'react'
+import React from 'react'
 import NavBar from '../components/NavBar'
 import IngredientInput from '../components/IngredientInput'
 import IngredientList from '../components/IngredientList'
-import Substitution from '../components/Substitution'
-function MealPlanner({data, inputString,handler,submitHandler}) {
+// import Substitution from '../components/Substitution'
+
+function MealPlanner({data, inputString,handler,submitHandler, onDebouncedChange}) {
     return (
         <div>
             <NavBar />
             <h1 className='text-4xl font-extrabold tracking-tight text-gray-900 sm:text-5xl md:text-6xl'>Meal Planner</h1>
             <p className='text-base text-gray-700 leading-relaxed font-serif p-4'>Plan your next meal by entering a main ingredient</p>
-            <IngredientInput inputData={data} inputHandler={handler} textVal={inputString} clickHandler={submitHandler}/>
+            <IngredientInput onDebouncedChange={onDebouncedChange} inputData={data} inputHandler={handler} textVal={inputString} clickHandler={submitHandler}/>
             {data.length>0? <IngredientList listItems={data}/>: null}
-            {console.log(data.length>0)}
-            <Substitution />
+        
+            {/* <Substitution /> */}
         </div>
     )
 }
