e673960d802f3f5d5d2f4132304c4153f1e19cdc
Mike MacDougall <mgmacdougall@outlook.com>
2025-09-07 12:34:35 -0400

    Add of recipelist router in index.js

--- api/src/index.js
+++ api/src/index.js
@@ -10,7 +10,8 @@ import morgan from 'morgan';
 import router from './routes/recipes.js';
 import substituteRouter from './routes/substitute.js'; // Uncomment this line to enable the substitute route
 import favouritesRouter from './routes/favourites.js'; // Uncomment this line to enable the favourites route
-import mealPlanRouter from './routes/mealPlanRouter.js'; // Uncomment this line to enable the meal plan route
+import mealPlanRouter from './routes/mealPlanRouter.js'; // Uncomment this line to enable the meal plan 
+import recipeListRouter from './routes/recipelistRouter.js';
 import userRouter from './routes/user.js';
 
 import { connectDB } from './utils/db.js'; // Uncomment this line to enable database connection
@@ -33,6 +34,7 @@ app.use('/recipes', router);
 app.use('/mealplan', mealPlanRouter); // Use the meal plan controller for handling meal plan routes
 app.use('/substitutes', substituteRouter); // Uncomment this line to enable the substitute route
 app.use('/favourites', favouritesRouter)
+app.use('/recipelist', recipeListRouter);
 app.use('/user',userRouter);
 app.listen(PORT, () => {
   logger.info(`Server is running on http://localhost:${PORT}`);
