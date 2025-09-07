bae967ff81af0531de6b3a4816b6ce6c2f90c90d
Mike MacDougall <mgmacdougall@outlook.com>
2025-09-07 13:24:04 -0400

    Add of comment to router list

--- api/src/index.js
+++ api/src/index.js
@@ -34,7 +34,7 @@ app.use('/recipes', router);
 app.use('/mealplan', mealPlanRouter); // Use the meal plan controller for handling meal plan routes
 app.use('/substitutes', substituteRouter); // Uncomment this line to enable the substitute route
 app.use('/favourites', favouritesRouter)
-app.use('/recipelist', recipeListRouter);
+app.use('/recipelist', recipeListRouter); // recipe list router to interact with spoontacular api.
 app.use('/user',userRouter);
 app.listen(PORT, () => {
   logger.info(`Server is running on http://localhost:${PORT}`);
