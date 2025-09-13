6188b705c1588c7bea8149f8a3c4d5939bc04580
Mike MacDougall <mgmacdougall@outlook.com>
2025-09-13 10:12:40 -0400

    Fixed page layout and onsubmit methodology

--- frontend/src/pages/Recipes.jsx
+++ frontend/src/pages/Recipes.jsx
@@ -2,14 +2,14 @@ import React from 'react'
 import RecipeCard from '../components/RecipeCard'
 import NavBar from '../components/NavBar'
 import RecipeForm from '../components/RecipeForm'
-function Recipes({ data, isVisible, formVisibleHandler, recipeFormTitleChangeHandler, recipeTitleData, handleQuantityChangeHandler, quantityData, handleIngredientChange, ingredientData, instructionsHandler, instructionsData , handleRecipeFormSubmit, recipeFavouriteHandler}) {
+function Recipes({ data, handleRecipeFormSubmit, recipeFavouriteHandler }) {
     return (
         <div>
             <NavBar />
             <h1 className='text-4xl font-extrabold tracking-tight text-gray-900 sm:text-5xl md:text-6xl'>Recipes</h1>
+            <RecipeForm handleRecipeFormSubmit={handleRecipeFormSubmit} />
             {data.length > 0 ? <RecipeCard data={data} recipeFavouriteHandler={recipeFavouriteHandler} /> : <h2>No recipes found</h2>}
-            <RecipeForm recipeTitleHandler={recipeFormTitleChangeHandler} recipeTitleData={recipeTitleData} recipeQuanityHandler={handleQuantityChangeHandler} quantityData={quantityData} ingredientChangeHandler={handleIngredientChange} ingredientData={ingredientData} instructionsHandler={instructionsHandler} instructionsData={instructionsData} handleRecipeFormSubmit={handleRecipeFormSubmit}/>
         </div>
     )
 }
-    export default Recipes
+export default Recipes
