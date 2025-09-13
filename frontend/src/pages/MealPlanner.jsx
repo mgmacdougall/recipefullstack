3a821055bccad66e74079e88fdb40540d9eb68fd
Mike MacDougall <mgmacdougall@outlook.com>
2025-09-13 11:07:10 -0400

    Refactor of MealPlanner page.

--- frontend/src/pages/MealPlanner.jsx
+++ frontend/src/pages/MealPlanner.jsx
@@ -1,20 +1,13 @@
 import React from 'react'
 import NavBar from '../components/NavBar'
-import IngredientInput from '../components/IngredientInput'
-import IngredientList from '../components/IngredientList'
-import IngredientListSearchButton from '../components/IngredientListSearchButton'
 
-function MealPlanner({data, inputString,handler,submitHandler, onDebouncedChange, ingredientListHandler, ingriendientListClickHandler}) {
+
+function MealPlanner() {
     return (
         <div>
             <NavBar />
             <h1 className='text-4xl font-extrabold tracking-tight text-gray-900 sm:text-5xl md:text-6xl'>Meal Planner</h1>
             <p className='text-base text-gray-700 leading-relaxed font-serif p-4'>Plan your next meal by entering a main ingredient</p>
-            <IngredientInput onDebouncedChange={onDebouncedChange} inputData={data} inputHandler={handler} textVal={inputString} clickHandler={submitHandler}/>
-            {data.length>0? <IngredientList listItems={data}/>: null}
-
-
-            {data.length>0? <IngredientListSearchButton buttonHandler={ingriendientListClickHandler}/>: null}
         </div>
     )
 }
